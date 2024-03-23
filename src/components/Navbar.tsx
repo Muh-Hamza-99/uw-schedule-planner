bde7d7ec49815e9445bbbb3e5e3d125bdba381b3
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-03-23 00:03:29 -0400

    Disable light/dark mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,12 @@
 import {
   Box,
   Flex,
-  Button,
   useColorModeValue,
   Stack,
-  useColorMode,
   Heading,
 } from "@chakra-ui/react";
-import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 const Navbar = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <>
       <Box borderRadius={2} bg={useColorModeValue("gray.100", "gray.900")} px={4}>
@@ -20,9 +16,6 @@ const Navbar = () => {
           </Box>
           <Flex alignItems={"center"}>
             <Stack direction={"row"} spacing={7}>
-              <Button onClick={toggleColorMode}>
-                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-              </Button>
             </Stack>
           </Flex>
         </Flex>
